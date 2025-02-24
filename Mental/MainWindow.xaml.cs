512839359081aad5ecb0ba77170d4e4d761564b0
mohcine-otmane <littlesandflea@gmail.com>
2025-02-24 23:18:40 +0000

    All is functional

--- Mental/MainWindow.xaml.cs
+++ Mental/MainWindow.xaml.cs
@@ -1,23 +1,92 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace Mental;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace Mental
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private int num1, num2, score = 0;
+        private Random random = new Random();
+        private DispatcherTimer timer;
+        private int timeLeft = 5;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeTimer();
+            GenerateNewProblem();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += TimerTick;
+        }
+
+        private void StartTimer()
+        {
+            timeLeft = 5;
+            TimerText.Text = $"Time Left: {timeLeft}s";
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            timeLeft--;
+
+            if (timeLeft > 0)
+            {
+                TimerText.Text = $"Time Left: {timeLeft}s";
+            }
+            else
+            {
+                timer.Stop();
+                ShowAnswer();
+            }
+        }
+
+        private void ShowAnswer()
+        {
+            ProblemText.Text = $"Answer: {num1 * num2}";
+            TimerText.Text = "Next Question in 1s...";
+
+            DispatcherTimer waitTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            waitTimer.Tick += (s, e) =>
+            {
+                waitTimer.Stop();
+                GenerateNewProblem();
+            };
+            waitTimer.Start();
+        }
+
+        private void GenerateNewProblem()
+        {
+            num1 = random.Next(1, 20);
+            num2 = random.Next(1, 20);
+            ProblemText.Text = $"{num1} × {num2}";
+            AnswerInput.Clear();
+            StartTimer();
+        }
+
+        private void SubmitAnswer(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(AnswerInput.Text, out int userAnswer) && userAnswer == num1 * num2)
+            {
+                score++;
+                ScoreText.Text = $"Score: {score}";
+                timer.Stop();
+                GenerateNewProblem();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect! Try again.", "Oops!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
     }
 }
--- Mental/obj/Debug/net8.0-windows/Mental.AssemblyInfo.cs
+++ Mental/obj/Debug/net8.0-windows/Mental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2fd90c1636adee26f481fb55322b2c8123d46ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8e95bbbe4c012ae05ca8d52208a54c313ce443")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
